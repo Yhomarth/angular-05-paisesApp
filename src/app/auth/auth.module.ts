a5ba8714bb850ffd6d4405220ee0ead10101612f
Yhomarth Reyes <yhomarth@gmail.com>
2021-11-19 19:01:48 -0400

    Clase 175 completada, Rutas hijas y LazyLoad - AuthRoutes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 
@@ -9,12 +10,13 @@ import { RegisterComponent } from './pages/register/register.component';
 @NgModule({
   declarations: [
 
-  
+
     LoginComponent,
         RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
